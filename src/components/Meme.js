8974f237ea1570137cd79ca1bb99479da317a010
Avishek Raut <avishekraut123@gmail.com>
2023-08-08 21:53:58 +0545

    Meme component update

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,11 @@
 export default function Meme() {
   return (
     <div className="meme-container">
-      <form className="form">
+      <div className="form">
         <input type="text" placeholder="Top Text" className="form--input" />
         <input type="text" placeholder="Bottom Text" className="form--input" />
         <button className="form--button">Get a new meme image 🖼</button>
-      </form>
+      </div>
     </div>
   );
 }
