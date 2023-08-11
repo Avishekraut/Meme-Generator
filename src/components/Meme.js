ba46d72b406777ba8ed96df057fe20a401c86ea0
Avishek Raut <avishekraut123@gmail.com>
2023-08-11 17:46:03 +0545

    Rendered meme image using state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+import memesData from "../memesData";
+
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("")
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url)
+  }
   return (
     <div className="meme-container">
       <div className="form">
         <input type="text" placeholder="Top Text" className="form--input" />
         <input type="text" placeholder="Bottom Text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
       </div>
+      <img className= "memeImage" src={memeImage}></img>
     </div>
   );
 }
